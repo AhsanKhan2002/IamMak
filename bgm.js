b5f6c62c3585d2abb025a24f9a64bdc878f767bb
ahsankhan2002 <ahsan_1805@hotmail.com>
2023-12-31 19:24:18 -0500

    first commit

---
+++ bgm.js
@@ -0,0 +1,14 @@
+// Your JavaScript code goes here
+// Your JavaScript code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    var audio = document.getElementById("backgroundMusic2");
+    if (audio) {
+      audio.volume = 0.03; // Set the volume to 50% (0.0 to 1.0)
+    }
+});
+document.addEventListener("DOMContentLoaded", function() {
+  var audio = document.getElementById("backgroundMusic");
+  if (audio) {
+    audio.volume = 0.02; // Set the volume to 50% (0.0 to 1.0)
+  }
+});
